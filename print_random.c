c6c9a32adc91c4ee17728591f2940196ebfefb39
stan39049 <stan39049@gmail.com>
2023-09-01 16:49:42 -0400

    Finished function in random.c

--- print_random.c
+++ print_random.c
@@ -9,7 +9,7 @@ int main()
 	int a;
 
 	/* seed the randomizer */
-	srand( (unsigned)time(NULL) );
+	srand((unsigned)time(NULL));
 
 	printf("Today's random word: ");
 	for(a=0;a<7;a++)
--- random.c
+++ random.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 char randchar(){
-
+    char letters[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return letters[rand() % 26];
 }
